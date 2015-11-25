edcc05f0f004f912c668e6eb45c90aa3b4f31098
Garrett Squire <garrett.squire@sendgrid.com>
2015-11-24 23:06:42 -0800

    update the example

--- examples/main.rs
+++ examples/main.rs
@@ -1,4 +1,5 @@
 extern crate sendgrid;
+extern crate time;
 
 use sendgrid::mail::Mail;
 use sendgrid::sg_client::SGClient;
@@ -9,16 +10,22 @@ fn main() {
     let api_key: String;
     match api_key_check {
         Some(key) => api_key = key.1,
-        None => panic!("Must supply API key in environment variables to use!")
+        None => panic!("Must supply API key in environment variables to use!"),
     }
 
     let sg = SGClient::new(api_key);
 
     let mut mail_info = Mail::new();
-    mail_info.add_to("some@email.com");
-    mail_info.add_from("another@email.com");
+    mail_info.add_to("you@example.com");
+    mail_info.add_from("some@some.com"):
     mail_info.add_subject("Rust is rad");
     mail_info.add_html("<h1>Hello from SendGrid!</h1>");
+    mail_info.add_from_name("Test");
+    mail_info.add_header("x-cool", "indeed");
+
+    let mut x_smtpapi = String::new();
+    x_smtpapi.push_str("{\"unique_args\":{\"test\":7}}");
+    mail_info.add_x_smtpapi(x_smtpapi);
 
     sg.send(mail_info);
 }
