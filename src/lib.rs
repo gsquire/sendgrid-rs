445c7941958f2f673c3805beb9a2a8482fb3c22d
Garrett Squire <garrettsquire@gmail.com>
2017-02-24 11:39:43 -0800

    bump minor version for release

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,12 @@
 #[macro_use]
 extern crate serde_derive;
 
+extern crate data_encoding;
 extern crate hyper;
 extern crate hyper_native_tls;
 extern crate serde;
 extern crate serde_json;
 extern crate url;
-extern crate data_encoding;
 
 pub mod sg_client;
 pub mod mail;
