d20df48576a42f8cc980626a7a02d86621675570
Richo Healey <richo@stripe.com>
2018-09-07 18:19:39 -0700

    Export Mail and SGClient from the root

--- src/lib.rs
+++ src/lib.rs
@@ -11,6 +11,10 @@ extern crate serde_json;
 extern crate url;
 
 pub mod errors;
-pub mod mail;
-pub mod sg_client;
+mod mail;
+mod sg_client;
 pub mod v3;
+
+pub use mail::Mail;
+pub use sg_client::SGClient;
+
