94dea8044eb80fdf15fb487e85131792ffe65b06
Richo Healey <richo@stripe.com>
2018-10-04 19:48:02 -0700

    Migrate from error chain to failure

--- src/errors.rs
+++ src/errors.rs
@@ -2,22 +2,18 @@ use std::io;
 
 use reqwest;
 use serde_json;
+use failure::Error;
 
-error_chain! {
-    types {
-        SendgridError, SendgridErrorKind, SendgridResultExt, SendgridResult;
-    }
-
-    foreign_links {
-        Io(io::Error);
-        JSONDecode(serde_json::Error);
-        ReqwestError(reqwest::Error);
-    }
-
-    errors {
-        InvalidFilename {
-            description("invalid filename")
-            display("could not UTF-8 decode this filename")
-        }
-    }
+#[derive(Fail, Debug)]
+pub enum SendgridError {
+    #[fail(display = "IO Error: {}", _0)]
+    Io(#[cause] io::Error),
+    #[fail(display = "JSON Error: {}", _0)]
+    JSONDecode(#[cause] serde_json::Error),
+    #[fail(display = "HTTP Error: {}", _0)]
+    ReqwestError(#[cause] reqwest::Error),
+    #[fail(display = "could not UTF-8 decode this filename")]
+    InvalidFilename,
 }
+
+pub type SendgridResult<T> = Result<T, Error>;
