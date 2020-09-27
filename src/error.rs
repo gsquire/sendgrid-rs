56d318264ef46ae9ca6e932bd753ebbd47e9d574
Garrett Squire <github@garrettsquire.com>
2020-09-27 08:10:35 -0700

    fix bad configuration

--- src/error.rs
+++ src/error.rs
@@ -6,7 +6,7 @@ use std::{
 use reqwest::{self, header::InvalidHeaderValue, StatusCode};
 use thiserror::Error as ThisError;
 
-/// Wrapper type which contains failed request's status code and body.
+/// Wrapper type which contains a failed request's status code and body.
 #[derive(Debug)]
 pub struct RequestNotSuccessful {
     status: StatusCode,
